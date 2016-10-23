99b4915f3c69bfda3b1e5bf6d9faa2f26f8916de
antonioterpin <antonio.terpin@gmail.com>
2016-10-24 00:37:51 +0200

    loading button

---
+++ TriviaPatente/app/src/main/java/com/ted_developers/triviapatente/app/utils/mViews/LoadingButton/LoadingButton.java
@@ -0,0 +1,63 @@
+package com.ted_developers.triviapatente.app.utils.mViews.LoadingButton;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import com.ted_developers.triviapatente.R;
+
+/**
+ * Created by Antonio on 23/10/16.
+ */
+public class LoadingButton extends Button implements ManageLoading {
+    private String prevText = "";
+    private ProgressBar progressBar;
+
+    public LoadingButton(Context context) {
+        super(context);
+    }
+
+    public LoadingButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LoadingButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void init() {
+        // create progress bar
+        progressBar = new ProgressBar(this.getContext());
+        // set relative layout params
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams((int) getResources().getDimension(R.dimen.loading_size), (int) getResources().getDimension(R.dimen.loading_size));
+        params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+        // add params
+        progressBar.setLayoutParams(params);
+        // set customized progress
+        progressBar.setIndeterminateDrawable(ContextCompat.getDrawable(this.getContext(), R.drawable.progress));
+        // add progress bar
+        ((RelativeLayout) this.getParent()).addView(progressBar);
+    }
+
+    @Override
+    public void startLoading() {
+        prevText = this.getText().toString();
+        this.setText("");
+        if (progressBar == null) {
+            init();
+        }
+    }
+
+    @Override
+    public void stopLoading() {
+        if(progressBar != null) {
+            progressBar.setVisibility(View.GONE);
+            this.setText(prevText);
+        }
+    }
+
+}
---
+++ TriviaPatente/app/src/main/java/com/ted_developers/triviapatente/app/utils/mViews/LoadingButton/ManageLoading.java
@@ -0,0 +1,9 @@
+package com.ted_developers.triviapatente.app.utils.mViews.LoadingButton;
+
+/**
+ * Created by Antonio on 24/10/16.
+ */
+public interface ManageLoading {
+    public void startLoading();
+    public void stopLoading();
+}
