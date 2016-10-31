cd5a6eb196fe39667d29a484eac8362d4745bff4
antonioterpin <antonio.terpin@gmail.com>
2016-10-31 21:45:20 +0100

    init socket and shared preferences

--- TriviaPatente/app/src/main/java/com/ted_developers/triviapatente/app/utils/mApplication.java
+++ TriviaPatente/app/src/main/java/com/ted_developers/triviapatente/app/utils/mApplication.java
@@ -3,6 +3,7 @@ package com.ted_developers.triviapatente.app.utils;
 import android.app.Application;
 
 import com.ted_developers.triviapatente.http.utils.RetrofitManager;
+import com.ted_developers.triviapatente.socket.modules.base.BaseSocketManager;
 
 /**
  * Created by Antonio on 22/10/16.
@@ -12,6 +13,8 @@ public class mApplication extends Application {
     public void onCreate() {
         super.onCreate();
         // init retrofit manager
-        RetrofitManager.init(getBaseContext());
+        RetrofitManager.init(this);
+        BaseSocketManager.init(this);
+        SharedTPPreferences.init(this);
     }
 }
