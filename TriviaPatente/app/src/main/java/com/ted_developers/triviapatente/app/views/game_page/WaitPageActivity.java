cb2fcfabd4fcbaab30d8063ddc18aed891a6162a
antonioterpin <antonio.terpin@gmail.com>
2016-12-06 23:02:29 +0100

    code clean up

---
+++ TriviaPatente/app/src/main/java/com/ted_developers/triviapatente/app/views/game_page/WaitPageActivity.java
@@ -0,0 +1,15 @@
+package com.ted_developers.triviapatente.app.views.game_page;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ted_developers.triviapatente.R;
+
+public class WaitPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wait_page);
+    }
+}
