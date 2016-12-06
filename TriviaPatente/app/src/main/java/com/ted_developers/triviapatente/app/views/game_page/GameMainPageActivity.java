da438c58d9e985bb64d43cac075510281ad78f27
antonioterpin <antonio.terpin@gmail.com>
2016-12-07 00:44:00 +0100

    choose category and wait page now are integrated

--- TriviaPatente/app/src/main/java/com/ted_developers/triviapatente/app/views/game_page/GameMainPageActivity.java
+++ TriviaPatente/app/src/main/java/com/ted_developers/triviapatente/app/views/game_page/GameMainPageActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.ted_developers.triviapatente.R;
 
-public class WaitPageActivity extends AppCompatActivity {
+public class GameMainPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wait_page);
+        setContentView(R.layout.activity_game_main_page);
     }
 }
