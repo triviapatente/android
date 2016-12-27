b9eb62499c4e7b1f622467ed9272e3ae65a789db
Antonio Terpin <antonio.terpin@gmail.com>
2016-12-27 23:38:36 +0100

    use of centralized son object builder in game socket manager

--- TriviaPatente/app/src/main/java/com/ted_developers/triviapatente/socket/modules/game/GameSocketManager.java
+++ TriviaPatente/app/src/main/java/com/ted_developers/triviapatente/socket/modules/game/GameSocketManager.java
@@ -1,8 +1,12 @@
 package com.ted_developers.triviapatente.socket.modules.game;
 
+import android.util.Log;
+import android.util.Pair;
+
 import com.ted_developers.triviapatente.app.utils.custom_classes.callbacks.SocketCallback;
 import com.ted_developers.triviapatente.models.auth.Hints;
 import com.ted_developers.triviapatente.models.responses.Success;
+import com.ted_developers.triviapatente.models.responses.SuccessAnsweredCorrectly;
 import com.ted_developers.triviapatente.models.responses.SuccessCategories;
 import com.ted_developers.triviapatente.models.responses.SuccessCategory;
 import com.ted_developers.triviapatente.models.responses.SuccessInitRound;
@@ -17,46 +21,44 @@ import org.json.JSONObject;
  */
 public class GameSocketManager extends BaseSocketManager {
     public void init_round(Long gameID, SocketCallback<SuccessInitRound> initRoundSocketCallback) {
-        try {
-            JSONObject data = new JSONObject();
-            data.put("game", gameID);
-            emit("init_round", data, SuccessInitRound.class, initRoundSocketCallback);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        emit("init_round",
+                buildJSONObject(
+                        new Pair<>("game", (Object) gameID)),
+                SuccessInitRound.class, initRoundSocketCallback);
     }
 
     public void get_proposed_categories(Long gameID, Long roundID, SocketCallback<SuccessCategories> proposedCategoriesCallback) {
-        try {
-            JSONObject data = new JSONObject();
-            data.put("round_id", roundID);
-            data.put("game", gameID);
-            emit("get_categories", data, SuccessCategories.class, proposedCategoriesCallback);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        emit("get_categories",
+                buildJSONObject(
+                        new Pair<>("game", (Object) gameID),
+                        new Pair<>("round_id", (Object) roundID)),
+                SuccessCategories.class, proposedCategoriesCallback);
     }
 
     public void choose_category(Long gameID, Long roundID, Long categoryID, SocketCallback<SuccessCategory> chooseCategoryCallback) {
-        try {
-            JSONObject data = new JSONObject();
-            data.put("round_id", roundID);
-            data.put("game", gameID);
-            data.put("category", categoryID);
-            emit("choose_category", data, SuccessCategory.class, chooseCategoryCallback);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        emit("choose_category",
+                buildJSONObject(
+                        new Pair<>("game", (Object) gameID),
+                        new Pair<>("round_id", (Object) roundID),
+                        new Pair<>("category", (Object) categoryID)),
+                SuccessCategory.class, chooseCategoryCallback);
     }
 
     public void get_questions(Long gameID, Long roundID, SocketCallback<SuccessQuizzes> getQuestionsCallback) {
-        try {
-            JSONObject data = new JSONObject();
-            data.put("round_id", roundID);
-            data.put("game", gameID);
-            emit("get_questions", data, SuccessQuizzes.class, getQuestionsCallback);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        emit("get_questions",
+                buildJSONObject(
+                        new Pair<>("game", (Object) gameID),
+                        new Pair<>("round_id", (Object) roundID)),
+                SuccessQuizzes.class, getQuestionsCallback);
+    }
+
+    public void answer(Long gameID, Long roundID, Long quizID, Boolean answer, SocketCallback<SuccessAnsweredCorrectly> answerCallback) {
+        emit("answer",
+                buildJSONObject(
+                        new Pair<>("game", (Object) gameID),
+                        new Pair<>("round_id", (Object) roundID),
+                        new Pair<>("quiz_id", (Object) quizID),
+                        new Pair<>("answer", (Object) answer)),
+                SuccessAnsweredCorrectly.class, answerCallback);
     }
 }
