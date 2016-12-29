0df3548e1ab70e898edb5fc6aafaf1ea8c83fe32
Antonio Terpin <antonio.terpin@gmail.com>
2016-12-29 15:58:36 +0100

    committo la soluzione funzionante per il press del play button

--- TriviaPatente/app/src/main/java/com/ted_developers/triviapatente/app/utils/custom_classes/buttons/PlayButton.java
+++ TriviaPatente/app/src/main/java/com/ted_developers/triviapatente/app/utils/custom_classes/buttons/PlayButton.java
@@ -3,16 +3,19 @@ package com.ted_developers.triviapatente.app.utils.custom_classes.buttons;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Parcelable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
 import com.ted_developers.triviapatente.R;
+import com.ted_developers.triviapatente.app.utils.mApplication;
 import com.ted_developers.triviapatente.app.views.game_page.GameMainPageActivity;
 import com.ted_developers.triviapatente.http.utils.RetrofitManager;
 import com.ted_developers.triviapatente.models.auth.User;
@@ -21,7 +24,7 @@ import com.ted_developers.triviapatente.models.game.Game;
 /**
  * Created by Antonio on 12/11/16.
  */
-public class PlayButton extends Button implements View.OnClickListener {
+public class PlayButton extends Button implements View.OnClickListener, View.OnTouchListener {
     // strings
     String playNow, newGame, details, endGame, contact;
     // drawables (different types of buttons)
@@ -59,6 +62,7 @@ public class PlayButton extends Button implements View.OnClickListener {
 
     private void bindElements(Context context) {
         setOnClickListener(this);
+        setOnTouchListener(this);
         // bind strings
         playNow = getResources().getString(R.string.play_now_button_text);
         newGame = getResources().getString(R.string.new_game_button_text);
@@ -86,6 +90,7 @@ public class PlayButton extends Button implements View.OnClickListener {
         this.setBackground(playNowDrawable);
         this.setText(playNow);
         this.setTextColor(playNowColor);
+        coloredColor = playNowColor;
     }
 
     // the button is a new game one
@@ -93,6 +98,7 @@ public class PlayButton extends Button implements View.OnClickListener {
         this.setBackground(newGameDrawable);
         this.setText(newGame);
         this.setTextColor(newGameColor);
+        coloredColor = newGameColor;
     }
 
     // the button is a summary one
@@ -100,6 +106,7 @@ public class PlayButton extends Button implements View.OnClickListener {
         this.setBackground(newGameDrawable);
         this.setText(endGame);
         this.setTextColor(newGameColor);
+        coloredColor = newGameColor;
     }
 
     // the button is a details one
@@ -107,6 +114,7 @@ public class PlayButton extends Button implements View.OnClickListener {
         this.setBackground(detailsDrawable);
         this.setText(details);
         this.setTextColor(detailsColor);
+        coloredColor = detailsColor;
     }
 
     // the button is a contact one
@@ -114,6 +122,7 @@ public class PlayButton extends Button implements View.OnClickListener {
         this.setBackground(contactDrawable);
         this.setText(contact);
         this.setTextColor(contactColor);
+        coloredColor = contactColor;
     }
 
     public void sendInvite(User element) {
@@ -127,7 +136,6 @@ public class PlayButton extends Button implements View.OnClickListener {
         new_game = false;
     }
 
-
     @Override
     public void onClick(View v) {
         Intent intent = new Intent(getContext(), GameMainPageActivity.class);
@@ -140,5 +148,22 @@ public class PlayButton extends Button implements View.OnClickListener {
             intent.putExtra(extraStringOpponent, RetrofitManager.gson.toJson(opponent));
         }
         getContext().startActivity(intent);
+    }
+
+    int coloredColor;
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                setTextColor(Color.WHITE);
+                setPressed(true);
+            } break;
+            case MotionEvent.ACTION_UP: {
+                if(mApplication.isPointInsideView((int) event.getX(), (int) event.getY(), this)) { performClick(); }
+                setTextColor(coloredColor);
+                setPressed(false);
+            } break;
+        }
+        return true;
     }
 }
