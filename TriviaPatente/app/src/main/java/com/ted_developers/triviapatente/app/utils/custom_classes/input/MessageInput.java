4d422f5aa3735d20df33c82e77eafb7933f49a54
Antonio Terpin <antonio.terpin@gmail.com>
2017-01-06 12:55:33 +0100

    alpha of message input (another shit)

---
+++ TriviaPatente/app/src/main/java/com/ted_developers/triviapatente/app/utils/custom_classes/input/MessageInput.java
@@ -0,0 +1,37 @@
+package com.ted_developers.triviapatente.app.utils.custom_classes.input;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by Antonio on 06/01/17.
+ */
+public class MessageInput extends RelativeLayout {
+    public MessageInput(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public MessageInput(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public MessageInput(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public MessageInput(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    private void init(Context context) {
+
+    }
+}
