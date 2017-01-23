6227f86eaf9821bacd01a170c866ecf4ef5f1a42
Antonio Terpin <antonio.terpin@gmail.com>
2017-01-23 22:28:30 +0100

    changed alpha view

--- TriviaPatente/app/src/main/java/com/ted_developers/triviapatente/app/views/AlphaView.java
+++ TriviaPatente/app/src/main/java/com/ted_developers/triviapatente/app/views/AlphaView.java
@@ -1,8 +1,10 @@
 package com.ted_developers.triviapatente.app.views;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.ted_developers.triviapatente.R;
+import com.ted_developers.triviapatente.app.utils.TPUtils;
 import com.ted_developers.triviapatente.app.utils.baseActivityClasses.TPActivity;
 
 public class AlphaView extends TPActivity {
@@ -11,5 +13,6 @@ public class AlphaView extends TPActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alpha_view);
+        ((TextView)findViewById(R.id.alphaTextView)).setText(TPUtils.translateEmoticons("Questa funzionalità sarà presto disponibile, continua a giocare! 0x1F60A"));
     }
 }
