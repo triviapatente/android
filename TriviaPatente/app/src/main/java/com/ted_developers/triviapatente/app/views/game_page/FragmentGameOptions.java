964be9b189265778b77b87688d1c981e04dffcfb
Antonio Terpin <antonio.terpin@gmail.com>
2017-01-23 20:56:11 +0100

    fix unvisible circle pager indicator

--- TriviaPatente/app/src/main/java/com/ted_developers/triviapatente/app/views/game_page/FragmentGameOptions.java
+++ TriviaPatente/app/src/main/java/com/ted_developers/triviapatente/app/views/game_page/FragmentGameOptions.java
@@ -29,9 +29,7 @@ import retrofit2.Call;
 import retrofit2.Response;
 
 public class FragmentGameOptions extends Fragment {
-    // TPGameActivity
     TPGameActivity activity;
-
 
     public FragmentGameOptions() {}
 
